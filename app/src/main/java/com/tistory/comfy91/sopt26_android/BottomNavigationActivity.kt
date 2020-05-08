a45d88558f1d73f073e574045c2f691b1f56de63
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-05-08 21:31:01 +0900

    BottomNavigationBar 와 ViewPager를 Adapter를 이용하여 연결

--- app/src/main/java/com/tistory/comfy91/sopt26_android/BottomNavigationActivity.kt
+++ app/src/main/java/com/tistory/comfy91/sopt26_android/BottomNavigationActivity.kt
@@ -2,7 +2,10 @@ package com.tistory.comfy91.sopt26_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 
 class BottomNavigationActivity : AppCompatActivity() {
@@ -11,10 +14,39 @@ class BottomNavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_navigation)
 
-
         // viewpager adapter
         bn_viewpager.adapter = MainPagerAdapter(supportFragmentManager)
         bn_viewpager.offscreenPageLimit = 2
+
+        // viewpagr와 bottom navigation view 연결
+        bottom_navigation_view.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.menu_home -> bn_viewpager.currentItem = 0
+                R.id.menu_book -> bn_viewpager.currentItem = 1
+                R.id.menu_person -> bn_viewpager.currentItem = 2
+            }
+            true
+        }
+
+        bn_viewpager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onPageSelected(position: Int) {
+                bottom_navigation_view.menu.getItem(position).isChecked = true
+
+            }
+
+        })
 
     }
 }
