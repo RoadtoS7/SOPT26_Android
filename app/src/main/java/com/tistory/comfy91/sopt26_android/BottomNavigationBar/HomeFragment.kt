fce8379d37b5af4ed823a345a50a7a2bb3d3cd0b
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-05-08 23:21:41 +0900

    RecyclerView clipToPadding, ItemDecoration 적용

--- app/src/main/java/com/tistory/comfy91/sopt26_android/BottomNavigationBar/HomeFragment.kt
+++ app/src/main/java/com/tistory/comfy91/sopt26_android/BottomNavigationBar/HomeFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tistory.comfy91.sopt26_android.R
 import com.tistory.comfy91.sopt26_android.RecyclerView.RecyclerViewAdapter
+import com.tistory.comfy91.sopt26_android.RecyclerView.RvItemDecoration
 import com.tistory.comfy91.sopt26_android.data.RvData
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -31,40 +32,44 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         recyclerViewAdapter = RecyclerViewAdapter(view.context)
         rv_home.adapter = recyclerViewAdapter
-        rv_home.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        rv_home.layoutManager =
+            LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        rv_home.addItemDecoration(RvItemDecoration(view.context))
         loadDatas()
     }
 
-    private fun loadDatas(){
-        datas.apply{
+    private fun loadDatas() {
+        datas.apply {
             add(
                 RvData(
                     "아리아나그란데",
                     "https://cdn.pixabay.com/photo/2020/05/03/21/10/heart-5126772__480.png",
                     "https://cdn.pixabay.com/photo/2020/04/22/21/14/spring-5080092__480.png"
-                ))
+                )
+            )
             add(
                 RvData(
                     "art",
                     "https://cdn.pixabay.com/photo/2020/04/09/03/32/mono-line-art-5019541__480.png",
                     "https://cdn.pixabay.com/photo/2020/04/20/08/27/fifties-records-5067136__480.png"
-                ))
+                )
+            )
             add(
                 RvData(
                     "초상화",
                     "https://cdn.pixabay.com/photo/2020/05/01/17/50/jesse-jackson-5118378__480.png",
                     "https://cdn.pixabay.com/photo/2019/04/10/17/01/watercolour-4117519__480.png"
-                ))
+                )
+            )
             add(
                 RvData(
                     "라마",
                     "https://cdn.pixabay.com/photo/2020/05/01/16/19/guanaco-5117901__480.png",
                     "https://cdn.pixabay.com/photo/2018/01/31/16/12/beach-3121393__480.png"
-                ))
+                )
+            )
         }
         recyclerViewAdapter.datas = datas
         recyclerViewAdapter.notifyDataSetChanged()
     }
-
-
 }
--- app/src/main/java/com/tistory/comfy91/sopt26_android/RecyclerView/RvItemDecoration.kt
+++ app/src/main/java/com/tistory/comfy91/sopt26_android/RecyclerView/RvItemDecoration.kt
@@ -6,20 +6,18 @@ import android.util.TypedValue
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class RvItemDecoration(private val context: Context): RecyclerView.ItemDecoration(){
+class RvItemDecoration(private val context: Context) : RecyclerView.ItemDecoration() {
     // 여백 사이즈 변수
-    private var size10: Int = 0
-    private var size5: Int = 0
+    private var size24: Int = 0
 
 
-     init{
-         size10 = dpToPx(context, 10F)
-         size5 = dpToPx(context, 5F)
-     }
+    init {
+        size24 = dpToPx(context, 24F)
+    }
 
     // dp -> pixel
     // view 사이즈에 변화를 주거나 여백을 줄 때는 pixel 로 변환해야 한다.
-    private fun dpToPx(context: Context, dp: Float): Int{
+    private fun dpToPx(context: Context, dp: Float): Int {
 
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
@@ -35,7 +33,7 @@ class RvItemDecoration(private val context: Context): RecyclerView.ItemDecoratio
         state: RecyclerView.State
     ) {
         super.getItemOffsets(outRect, view, parent, state)
-        outRect.top = size5
+        outRect.bottom = size24
     }
 
 
