0996c76167e20d924be3dd1730a7fd319f8a4f1f
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-05-10 20:47:30 +0900

    ServerRepository 내용 완성

--- app/src/main/java/com/tistory/comfy91/sopt26_android/retrofitdata/ServerRespository.kt
+++ app/src/main/java/com/tistory/comfy91/sopt26_android/retrofitdata/ServerRespository.kt
@@ -1,2 +1,31 @@
 package com.tistory.comfy91.sopt26_android.retrofitdata
 
+import com.tistory.comfy91.sopt26_android.retrofit.RetrofitInterface
+import com.tistory.comfy91.sopt26_android.retrofit.RetrofitObj
+import retrofit2.Call
+
+class ServerRespository : RetrofitInterface{
+    override fun requestSignUp(
+        id: String,
+        pw: String,
+        name: String,
+        email: String,
+        phone: String
+    ): Call<ResponseSignUp> {
+        return RetrofitObj
+            .service
+            .requestSignUp(
+                id,
+                pw,
+                name,
+                email,
+                phone
+        )
+    }
+
+    override fun requestSignIn(id: String, pw: String): Call<ResponseSignIn> {
+        return RetrofitObj.service
+            .requestSignIn(id, pw)
+    }
+
+}
